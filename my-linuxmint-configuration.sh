ad8e35c5f2e8476bc16a22b3ef61e615a3e86af8
Álysson Gleyson <alyssongleysondasilva@gmail.com>
2025-05-28 18:36:38 -0300

    feat: create base script fot Linux Mint system setup

---
+++ my-linuxmint-configuration.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+###################################################################
+# Script Name:	my-linuxmint-configuration.sh
+# Description:	My post-installation configuration of Linux Mint
+# Args:		bash my-linuxmint-configuration.sh
+# Author:	Álysson Gleyson da Silva
+###################################################################
+
